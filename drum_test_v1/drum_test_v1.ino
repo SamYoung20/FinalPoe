cee86e0aeb2215dc53cd2be8a736a57afd0fd595
GGRice <gretchrice4@gmail.com>
2017-10-20 08:51:07 -0400

    first test code

---
+++ drum_test_v1/drum_test_v1.ino
@@ -0,0 +1,13 @@
+int striketime = 3000; // the length of time the motor will be on
+int resttime = 1000; // the length of rest between strikes
+
+void setup() {
+  pinMode(8, OUTPUT); // set the output pins
+}
+
+void loop() {
+  digitalWrite(8, HIGH); // turn on motor 1
+  delay(striketime);
+  digitalWrite(8, LOW); // turn off motor 1
+  delay(resttime); // wait
+}
