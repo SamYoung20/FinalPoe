42af18ccb6cb82b0acc87f666cb4d258d4c283e1
Emma Westerhoff <emma.westerhoff@students.olin.edu>
2017-12-08 00:19:15 -0500

    preliminary calibration code

--- drum_test_v6_sense/drum_test_v6_sense.ino
+++ drum_test_v6_sense/drum_test_v6_sense.ino
@@ -25,6 +25,8 @@ int t1, t2 = 0; //time variable to avoid use of delays
 
 int lis = 2; //how many beats should listen for
 
+int start_time = 0; //timing for processing time calibration
+
 //takes absolute value of a long
 void absolute(long x){
   if(x<0){
@@ -37,46 +39,48 @@ void absolute(long x){
 
 //function to find period from beat detection 
 void findPeriod(){
-  Serial.println("In findPeriod");
+  start_time = millis();
   long avgPeriod = 0;
   for(int i = 1; i<=lis; i++){
-    Serial.println(i);
     pin_val = digitalRead(beat_pin); //beat detection reading
     
     long dur_on = pulseIn(beat_pin, HIGH, 3000000); //duration beat sounds
     absolute(dur_on);
     while(dur_on < 100000){
-      Serial.println("dur too low");
-      Serial.println(dur_on);
       long dur_on = pulseIn(beat_pin, HIGH, 3000000); //duration beat sounds
       absolute(dur_on);
     }
-    Serial.println(dur_on);
-    
+   
     long dur_off = pulseIn(beat_pin, LOW, 3000000); //duration between beats
     absolute(dur_off);
-    Serial.println(dur_off);
+
     while(dur_off < 100000){
       long dur_off = pulseIn(beat_pin, LOW, 3000000); //duration beat sounds
-      absolute(dur_on);
+      absolute(dur_off);
     }
 
     period = period + dur_on + dur_off; //total period (all together) to allow for averaging
-    Serial.println(period);
     avgPeriod = period/long(i); //averages to find period
-    Serial.println(avgPeriod);
-
   }
 
   //finds the correct period and bpm from the reading
-  period = avgPeriod/1000.0; //finds period
-  Serial.println(period);
-  Serial.println(period, 7);
+  period = avgPeriod/1000.0; //period to milliseconds
   bpm = 60000/period; //finds bpm with read period
+  
+  int end_time = millis();
+  int run_time = end_time - start_time;
+
+  float periods_taken = float(run_time)/float(period);
+  int ideal_time = (int(periods_taken)+1)*period;
+
+  int delay_time = ideal_time-run_time;
+  
+  delay(delay_time);
+  int final_end_time = millis();
+  Serial.println(final_end_time-start_time);
+
   bpm = int(bpm); //turns bpm to int since bpm is never not an int
   period = float(60000)/float(bpm); //finds proper period from corrected bpm
-  Serial.println(period);
-  
 }
 
 void setup() {
@@ -96,6 +100,7 @@ void loop() {
     state = Serial.read(); //0 or 1
     Serial.write(state);
     if(state == 'f'){ //finds beat if type 'f'
+      start_time = millis();
       findPeriod(); //finds function to find period
       bpm = 60000/period; //finds the bpm, easier to compare with bpm
       Serial.println(bpm);
