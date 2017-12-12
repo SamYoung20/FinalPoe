a751f4896b84c602f9f49e4e880fa9785cf7ce2f
Emma Westerhoff <emma.westerhoff@students.olin.edu>
2017-12-11 20:59:36 -0500

    Mechanical arm calibration code

--- drum_test_v9_thinkAct/drum_test_v9_thinkAct.ino
+++ drum_test_v9_thinkAct/drum_test_v9_thinkAct.ino
@@ -1,6 +1,6 @@
 /**
    Takes bpm from sense Arduino to determine how to hit the drum.
-   No user input
+   Requires user input p
    Will diplay when and what diplaying and strike and rest times
 */
 
@@ -29,22 +29,77 @@ int t1, t2 = 0; //time variable to avoid use of delays
 float st_quarter, rt_quarter = 0; //strike time and rest time quarter note length from period
 int rhythm = 1; //original rhythm to play with no user input
 
+int calibrateTime = 1;
+
+void setup() {
+  //start Serials and motor shield
+  Serial.begin(115200);
+  mySerial.begin(14400);
+  AFMS.begin();
+
+  //set initial speeds for motors
+  m1->setSpeed(150); //arm
+  m2->setSpeed(100); //foot
+
+}
+
+void loop() {
+  //if (mySerial.available() > 0) { //if serial value is input, read it
+      if(Serial.read() == 'p'){
+      //bpm = mySerial.read(); //takes bpm from sense Arduino
+      bpm = 75;
+      period = float(60000) / float(bpm);
+      Serial.println(bpm);
+
+    rhythm = 0;
+      }
+    //run arm and/or foot
+    if(Serial.read() == 'f')
+      calibrateTime = calibrateTime+5;
+    if(Serial.read() == 'd')
+      calibrateTime = calibrateTime-5;
+      
+    hand(m1, period);
+
+    //foot(m1, 1023, 341, rt_quarter);
+  //}
+}
 
 
 //function to make a hit, params: motor, strike and rest time,
-void beat(Adafruit_DCMotor *m, int st, int rt) {
-
+//runs motor forwards, then backwards, then waits
+void hand(Adafruit_DCMotor *m, float period) {
+  float fd = 193; //determine forward time
+  //Serial.println(fd);
+  int bk = 62; //determine back time
+  int brkTime = 40; //determine rest time
+  int rt = period - fd - bk - brkTime; //determine rest time
+  
   //t1 and t2 used to avoid use of delay
   t2 = millis();
-  while (t2 - t1 <= st) { //turns motor on for length of st
+  while (t2 - t1 <= fd) { //turns motor on for length of fd
     m->run(FORWARD); //turns motor on
     t2 = millis();
   }
   t1 = t2;
 
   t2 = millis();
-  while (t2 - t1 <= rt) { //turns motor off for length of rt
+  while (t2 - t1 <= brkTime) { //turns motor on for length of fd
+    m->run(RELEASE); //turns motor on
+    t2 = millis();
+  }
+  t1 = t2;
+  
+  t2 = millis();
+  while (t2 - t1 <= bk) { //turns motor backwards for length of bk
     m->run(BACKWARD); // turns motor backwards
+    t2 = millis();
+  }
+  t1 = t2;
+
+  t2 = millis();
+  while (t2 - t1 <= rt) { //turns motor off for length of rt
+    m->run(RELEASE);
     t2 = millis();
   }
   t1 = t2;
@@ -70,109 +125,8 @@ void foot(Adafruit_DCMotor *m, int up, int down, int rt) {
 }
 */
 
-void setup() {
-  //start Serials and motor shield
-  Serial.begin(115200);
-  mySerial.begin(14400);
-  AFMS.begin();
-
-  //set initial speeds for motors
-  m1->setSpeed(100); //arm
-  m2->setSpeed(100); //foot
-
-}
-
-void loop() {
-  //Serial.println("hi");
-  if (mySerial.available() > 0) { //if serial value is input, read it
-      if(Serial.read() == 'p'){
-      Serial.println("Receiving");
-      bpm = mySerial.read(); //takes bpm from sense Arduino
-      period = float(60000) / float(bpm);
-      //Serial.println("****************************");
-      Serial.println(bpm);
-
-    st_quarter = float(.2) * float(period); //determines strike time
-    rt_quarter = float(.8) * float(period); //determine rest time
-
-    rhythm = 0;
-      }
-      //beat(m1, st_quarter, rt_quarter);
-  }
-}
 
 
-  //Serial.println(st_quarter);
-  //Serial.println(rt_quarter);
-
-  //to run arm, uncomment this one
-  //beat(m1, st_quarter, rt_quarter);
-  //foot(m1, 1023, 341, rt_quarter);
-
-
-/*
-  switch (rhythm) { //switches between 7 different rhythms depending on what variable rhythm equals
-    case 0: //quarter notes
-      Serial.println("Case 0");
-      foot(m2, 1023, 0);
-      //st_quarter = rt_quarter; //for foot, still messing with this
-      //rt_quarter=0;
-      //beat(m1, st_quarter, rt_quarter);
-
-
-      break;
-      //
-      //    case 1: //eigth notes
-      //      //Serial.println("Case 1");
-      //      beat(m1, st_quarter/2, rt_quarter/2);
-      //      break;
-      //
-      //    case 2: //sixteenth notes
-      //      //Serial.println("Case 2");
-      //      beat(m1, st_quarter/4, rt_quarter/4);
-      //      break;
-      //
-      //    case 3: //eigth+sixteenth
-      //      beat(m1, 100, 400);
-      //      beat(m1, 50, 200);
-      //      break;
-      //
-      //    case 4: //eighth+sixteenth+sixteenth
-      //      beat(m1, 100, 400);
-      //      beat(m1, 50, 200);
-      //      beat(m1, 50, 200);
-      //      break;
-      //
-      //    case 5://e+s+s+e+s
-      //      beat(m1, 100, 400);
-      //      beat(m1, 50, 200);
-      //      beat(m1, 50, 200);
-      //      beat(m1, 100, 400);
-      //      beat(m1, 50, 200);
-      //      break;
-      //
-      //    case 6://1++3+4
-      //      beat(m1, 100, 400);
-      //      beat(m1, 100, 800);
-      //      beat(m1, 100, 400);
-      //      beat(m1, 100, 400);
-      //      beat(m1, 100, 400);
-      //      beat(m1, 100, 800);
-      //      break;
-      //
-      //    case 7: //foot and hand
-      //      beat(m1, st_quarter, rt_quarter); //hand
-      //      beat(m2, st_quarter, rt_quarter); //foot
-      //      break;
-      //
-      //    case 8: // don't move
-      //      Serial.print("Outside Range");
-      //      digitalWrite(m1, LOW);
-      //      break;
-
-  }
-  */
-//}
 
 
 
--- testFreq/testFreq.ino
+++ testFreq/testFreq.ino
@@ -1,10 +1,10 @@
 #include <SoftwareSerial.h>
 unsigned int data =0;
-const int samples = 5;
+const int samples = 3;
 const unsigned int debounce = 350;
 const int threshold = 100;
 float rolling[samples]; // global variable will keep track of relevant samples
-const int inp = 2;
+const int inp = A2;
 unsigned long lastTime;
 SoftwareSerial mySerial(11, 10);
 void setup() {
@@ -14,11 +14,6 @@ void setup() {
 }
 
 void loop() {
-  //if(Serial.available() > 0) {                      //checks for input from Serial
-   // if(Serial.read() == 'f'){                               //finds beat if type 'f'
-        
-   // }
-  //}
   int sense = analogRead(inp);
   unsigned long revTime;
   if(sense < threshold && (unsigned long)(millis() - lastTime) > debounce){         // close to negative rail (is it a beat) and is a new beat (debounce)
@@ -28,33 +23,25 @@ void loop() {
     unsigned int avg = updateAverage(freq);
     data = avg;
     Serial.println(data);
-    mySerial.write(data); 
-    
-    //printArray(rolling,5);
-    //Serial.println(avg);
-    
-    
+    mySerial.write(data);
   }
 }
 
 float updateAverage(float newVal){
   float tempRolling[samples];
-
   tempRolling[samples - 1] = newVal; // the last element is the new value
 
-  for(int i = 0; i < samples - 1; i ++){ // copy the last elements of the old array into the beginning of the new array
+  for(int i = 0; i < samples - 1; i ++) // copy the last elements of the old array into the beginning of the new array
     tempRolling[i] = rolling[i+1];
-  }
 
-  for(int i = 0; i < samples; i ++){ // overwrite the old array with the new one
+  for(int i = 0; i < samples; i ++) // overwrite the old array with the new one
     rolling[i] = tempRolling[i];
-  }
+  
   unsigned long sum = 0;
-  for(int i = 0; i < samples; i ++){ // sum the elements
+  for(int i = 0; i < samples; i ++) // sum the elements
     sum += rolling[i];
-  }
+  
   unsigned int avg = round(sum/samples);
-
   return avg;
 }
 
@@ -62,16 +49,6 @@ float convertUnits(unsigned long input){
   //to Hz:
   float freq = 60000.00/input; //micros
   return freq;
-  /*
-  //MPH:
-  float circ = wheelDia * 3.1416;
-  int speedMPH = (freq * 3600 * circ)/63360;
-  return speedMPH;
-  /*
-  //RPM:
-  unsigned int RPM = input * 60 * 2;
-  return RPM;
-  */
 }
 
 void printArray(float arr[], int len){ // useful for debugging
