6c84d124cc87f0bbcf86494a36600846a2a72795
Gretchen <gretchen.rice@students.olin.edu>
2017-12-11 22:11:53 -0500

    Manual calibration done

--- drum_test_v9_thinkAct/drum_test_v9_thinkAct.ino
+++ drum_test_v9_thinkAct/drum_test_v9_thinkAct.ino
@@ -31,6 +31,8 @@ int rhythm = 1; //original rhythm to play with no user input
 
 int calibrateTime = 1;
 
+char user_in = 'o';
+
 void setup() {
   //start Serials and motor shield
   Serial.begin(115200);
@@ -45,22 +47,29 @@ void setup() {
 
 void loop() {
   //if (mySerial.available() > 0) { //if serial value is input, read it
-      if(Serial.read() == 'p'){
-      //bpm = mySerial.read(); //takes bpm from sense Arduino
-      bpm = 75;
-      period = float(60000) / float(bpm);
-      Serial.println(bpm);
-
-    rhythm = 0;
+    if (Serial.available())
+      user_in = Serial.read();
+    else
+      user_in = 'o';
+     
+      if(user_in == 'p'){
+        //bpm = mySerial.read(); //takes bpm from sense Arduino
+        bpm = 75;
+        period = float(60000) / float(bpm);
+        Serial.println(bpm);
+  
+        rhythm = 0;
       }
+    
     //run arm and/or foot
-    if(Serial.read() == 'f')
-      calibrateTime = calibrateTime+5;
-    if(Serial.read() == 'd')
-      calibrateTime = calibrateTime-5;
+    if(user_in == 'f')
+      calibrateTime = 5;
+    else if(user_in == 'd')
+      calibrateTime = -5;
+    else
+      calibrateTime = 0;
       
-    hand(m1, period);
-
+    hand(m1, period, calibrateTime);
     //foot(m1, 1023, 341, rt_quarter);
   //}
 }
@@ -68,12 +77,13 @@ void loop() {
 
 //function to make a hit, params: motor, strike and rest time,
 //runs motor forwards, then backwards, then waits
-void hand(Adafruit_DCMotor *m, float period) {
+void hand(Adafruit_DCMotor *m, float period, int calibrateTime) {
   float fd = 193; //determine forward time
-  //Serial.println(fd);
+  Serial.println(fd);
   int bk = 62; //determine back time
   int brkTime = 40; //determine rest time
-  int rt = period - fd - bk - brkTime; //determine rest time
+  int rt = period - fd - bk - brkTime + calibrateTime; //determine rest time
+  //add or subtract to rest time here temporarily
   
   //t1 and t2 used to avoid use of delay
   t2 = millis();
@@ -127,87 +137,7 @@ void foot(Adafruit_DCMotor *m, int up, int down, int rt) {
 
 
 
-<<<<<<< HEAD
-  
-    st_quarter = float(.2)*float(period); //determines strike time
-    rt_quarter = float(.8)*float(period); //determine rest time
 
-    rhythm = 0;
-  }
-
-  
-  //Serial.println(st_quarter);
-  //Serial.println(rt_quarter);
-
-//to run arm, uncomment this one
-  //beat(m1, st_quarter, rt_quarter);
-  foot(m1, 682, 341, rt_quarter);
-  
-
-  
-  switch(rhythm){ //switches between 7 different rhythms depending on what variable rhythm equals
-    case 0: //quarter notes
-      Serial.println("Case 0");
-      foot(m2, 1023, 0);
-      //st_quarter = rt_quarter; //for foot, still messing with this
-      //rt_quarter=0;
-      //beat(m1, st_quarter, rt_quarter);
-      
-      
-      break;
-//  
-//    case 1: //eigth notes
-//      //Serial.println("Case 1");
-//      beat(m1, st_quarter/2, rt_quarter/2);
-//      break;
-//  
-//    case 2: //sixteenth notes
-//      //Serial.println("Case 2");
-//      beat(m1, st_quarter/4, rt_quarter/4);
-//      break;
-//  
-//    case 3: //eigth+sixteenth
-//      beat(m1, 100, 400);
-//      beat(m1, 50, 200);
-//      break;
-//  
-//    case 4: //eighth+sixteenth+sixteenth
-//      beat(m1, 100, 400);
-//      beat(m1, 50, 200);
-//      beat(m1, 50, 200);     
-//      break;
-//  
-//    case 5://e+s+s+e+s
-//      beat(m1, 100, 400);
-//      beat(m1, 50, 200);
-//      beat(m1, 50, 200); 
-//      beat(m1, 100, 400);
-//      beat(m1, 50, 200);
-//      break;
-//  
-//    case 6://1++3+4
-//      beat(m1, 100, 400);
-//      beat(m1, 100, 800);
-//      beat(m1, 100, 400);
-//      beat(m1, 100, 400);
-//      beat(m1, 100, 400);
-//      beat(m1, 100, 800);
-//      break;  
-//
-//    case 7: //foot and hand
-//      beat(m1, st_quarter, rt_quarter); //hand
-//      beat(m2, st_quarter, rt_quarter); //foot
-//      break;
-//  
-//    case 8: // don't move
-//      Serial.print("Outside Range");
-//      digitalWrite(m1, LOW);
-//      break;
-        
-  }
-}
-=======
->>>>>>> a751f4896b84c602f9f49e4e880fa9785cf7ce2f
 
 
 
