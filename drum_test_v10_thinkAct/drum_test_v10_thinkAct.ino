1803ebbfdacb725ba6dafa3b593204ebf951bfbe
Emma Westerhoff <emma.westerhoff@students.olin.edu>
2017-12-12 23:44:19 -0500

    foot calibration code

---
+++ drum_test_v10_thinkAct/drum_test_v10_thinkAct.ino
@@ -0,0 +1,184 @@
+/**
+   Takes bpm from sense Arduino to determine how to hit the drum.
+   Requires user input p
+   Will diplay when and what diplaying and strike and rest times
+*/
+
+//include all libraries
+#include <SoftwareSerial.h>
+#include <Wire.h>
+#include <Adafruit_MotorShield.h>
+#include "utility/Adafruit_MS_PWMServoDriver.h"
+
+
+//Sets up Software serial and motor shield
+SoftwareSerial mySerial(11, 10);
+Adafruit_MotorShield AFMS = Adafruit_MotorShield();
+
+//sets up motors
+Adafruit_DCMotor *m1 = AFMS.getMotor(1); //hand motor
+Adafruit_DCMotor *m2 = AFMS.getMotor(2); //foot motor
+
+int POT = A0;
+
+int bpm = 0; //beats per minute, calculated using period
+float period = 0; //length of period, found from beat detection
+
+int t1, t2 = 0; //time variable to avoid use of delays
+
+float st_quarter, rt_quarter = 0; //strike time and rest time quarter note length from period
+int rhythm = 1; //original rhythm to play with no user input
+
+int calibrateTime = 1;
+
+char user_in = 'o';
+char drum_status = 's';
+
+void setup() {
+  //start Serials and motor shield
+  Serial.begin(115200);
+  mySerial.begin(14400);
+  AFMS.begin();
+
+  //set initial speeds for motors
+  //m1->setSpeed(150); //arm
+  //m1->setSpeed(150); //arm
+  m1->setSpeed(70); //foot
+
+}
+
+void loop() {
+  //if (mySerial.available() > 0) { //if serial value is input, read it
+    if (Serial.available()){
+      user_in = Serial.read(); }
+    else
+      user_in = 'o';
+   
+      if(user_in == 'p'){
+        //bpm = mySerial.read(); //takes bpm from sense Arduino
+        bpm = 90;
+        Serial.println(bpm);
+        drum_status = 'g';
+        period = float(60000) / float(bpm);
+        rhythm = 0;
+      }
+
+    if (user_in == 's')
+      drum_status = 's';
+      
+    //run arm and/or foot
+    if(user_in == 'f'){
+      calibrateTime = 5;
+      Serial.println(calibrateTime);}
+    else if(user_in == 'd'){
+      calibrateTime = -5;
+      Serial.println(calibrateTime);}
+    else
+      calibrateTime = 0;
+
+  int ana_in = analogRead(POT);
+  Serial.println(ana_in);
+  
+    if (drum_status == 'g') {
+      //hand(m1, period, calibrateTime);
+      ana_in = analogRead(POT);
+      Serial.println(ana_in);
+      foot(m1, period, calibrateTime); }
+  //}
+}
+
+//function to make a hit, params: motor, strike and rest time,
+//runs motor forwards, then backwards, then waits
+void hand(Adafruit_DCMotor *m, float period, int calibrateTime) {
+  float fd = 253; //determine forward time
+  int bk = 62; //determine back time
+  int brkTime = 40; //determine rest time
+  int rt = period - fd - bk - brkTime + calibrateTime; //determine rest time
+  //add or subtract to rest time here temporarily
+  
+  //t1 and t2 used to avoid use of delay
+  t2 = millis();
+  while (t2 - t1 <= fd) { //turns motor on for length of fd
+    m->setSpeed(130);
+    m->run(FORWARD); //turns motor on
+    t2 = millis();
+  }
+  t1 = t2;
+
+  t2 = millis();
+  while (t2 - t1 <= brkTime) { //turns motor on for length of fd
+    m->run(RELEASE); //turns motor on
+    t2 = millis();
+  }
+  t1 = t2;
+  
+  t2 = millis();
+  while (t2 - t1 <= bk) { //turns motor backwards for length of bk
+    m->setSpeed(160);
+    m->run(BACKWARD); // turns motor backwards
+    t2 = millis();
+  }
+  t1 = t2;
+
+  t2 = millis();
+  while (t2 - t1 <= rt) { //turns motor off for length of rt
+    m->run(RELEASE);
+    t2 = millis();
+  }
+  t1 = t2;
+}
+
+//function to run the foot
+
+void foot(Adafruit_DCMotor *m, float period, int calibrateTime) {
+  int up = 200;
+  int down = 630;
+
+  int ana_in = analogRead(POT);
+  Serial.println(ana_in);
+
+  int foot_start = millis();
+  t2 = millis();
+  while (analogRead(POT) < down) { //turns motor off for length of rt
+    ana_in = analogRead(POT);
+  Serial.println(ana_in);
+    m1->run(FORWARD);
+    t2 = millis();
+  }
+  t1 = t2;
+
+  t2 = millis();
+  while (t2 - t1 <= 500) { //turns motor off for length of rt
+    m1->run(RELEASE);
+    t2 = millis();
+  }
+  t1 = t2;
+
+  t2 = millis();
+  while (analogRead(POT) > up) { //turns motor off for length of rt
+    ana_in = analogRead(POT);
+  Serial.println(ana_in);
+    m1->run(BACKWARD);
+    t2 = millis();
+  }
+  t1 = t2;
+
+  int foot_end = millis();
+  int foot_time = foot_end-foot_start;
+  
+  t2 = millis();
+  while (t2 - t1 <= period-foot_time) { //turns motor off for length of rt
+    m1->run(RELEASE);
+    t2 = millis();
+  }
+  t1 = t2;
+}
+
+
+
+
+
+
+
+
+
--- testMotor/testMotor.ino
+++ testMotor/testMotor.ino
@@ -1,27 +1,111 @@
-// change this value to match your striketime
-int striketime = 100; // the length of time the motor will be on
+//include all libraries
+#include <SoftwareSerial.h>
+#include <Wire.h>
+#include <Adafruit_MotorShield.h>
+#include "utility/Adafruit_MS_PWMServoDriver.h"
 
-// will hold the delay times for various beats
-int fastbeat = 0;
-int beat = 0;
-int slowbeat = 0;
+
+//Sets up Software serial and motor shield
+SoftwareSerial mySerial(11, 10);
+Adafruit_MotorShield AFMS = Adafruit_MotorShield();
+
+//sets up motors
+Adafruit_DCMotor *m1 = AFMS.getMotor(1); //hand motor
+Adafruit_DCMotor *m2 = AFMS.getMotor(4); //foot motor
+
+int POT = A0;
+
+int bpm = 0; //beats per minute, calculated using period
+float period = 0; //length of period, found from beat detection
+
+int t1, t2 = 0; //time variable to avoid use of delays
+
+float st_quarter, rt_quarter = 0; //strike time and rest time quarter note length from period
+int rhythm = 1; //original rhythm to play with no user input
+
+int calibrateTime = 1;
+
+int time1, time2, time3, time4, time5, time6, time7 = 0
+
+char user_in = 'o';
+char drum_status = 's';
+
 void setup() {
-pinMode(7, OUTPUT); // set the output pins
-pinMode(8, OUTPUT); // make sure these pinnumbers match your wiring
-// set the delay times
-fastbeat = 150 - striketime;
-beat = 300 - striketime;
-slowbeat = 600 - striketime;
+  //start Serials and motor shield
+  Serial.begin(115200);
+  mySerial.begin(14400);
+  AFMS.begin();
+
+  //set initial speeds for motors
+  m1->setSpeed(150); //arm
+  m2->setSpeed(100); //foot
+
 }
+
 void loop() {
-digitalWrite(8, HIGH);
-delay(striketime);
-digitalWrite(8, LOW);
-delay(beat);
-digitalWrite(8, HIGH);
-delay(striketime);
-digitalWrite(8, LOW);
-delay(beat);
+  if (Serial.available()){
+    user_in = Serial.read(); }
+  else
+    user_in = 'o';
+   
+  if(user_in == 'p'){
+    bpm = 60;
+    Serial.println(bpm);
+    period = float(60000) / float(bpm);
+    drum_status = 'g';
+    rhythm = 0;
+   }
+    
+  if(user_in == 'f'){
+    calibrateTime = 5;
+    Serial.println(calibrateTime);}
+  else if(user_in == 'd'){
+    calibrateTime = -5;
+    Serial.println(calibrateTime);}
+  else
+    calibrateTime = 0;
+
+  if(user_in == 's'){
+    drum_status = 's';
+  }
+  if (drum_status == 'g')
+    beat(m1, m2, period, calibrateTime);
+}
+
+void beat(Adafruit_DCMotor *m1, Adafruit_DCMotor *m2, float period, int calibrateTime) {
+  float fd = 193; //determine forward time
+  int bk = 62; //determine back time
+  int brkTime = 40; //determine switch direction time
+  int footTime = 62; //stand in for foot down time 
+  int rt = period - fd - bk - brkTime + calibrateTime; //determine rest time
+
+  //calibrated arm values
+  int arm_up = 193;
+  int arm_break = 40;
+  int arm_hit = 62;
+  int arm_remainder = period - arm_up - arm_break - arm_hit;
+
+  track2 = millis();
+  if ((track2 - track1 <= 
+    t2 = millis();
+    while (t2 - t1 <= wait_time) 
+         m1->run(FORWARD);
+    t1 = t2;
+  
+  
+}
+
+void wait(char command1, int wait_time){
+  t2 = millis();
+  while (t2 - t1 <= wait_time) { 
+    if (command1 == 'o')
+       m1->run(RELEASE);
+    else if (command1 == 'f')
+       m1->run(FORWARD);
+    else if (command1 == 'r')
+       m1->run(BACKWARD);
+  }
+  t1 = t2;
 }
 
 
